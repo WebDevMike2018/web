5168ce2cc3d0e7fa28e69a1c59b6e69d42e362e2
WebDevMike2018 <75060355+WebDevMike2018@users.noreply.github.com>
2022-07-29 16:21:26 +0800

    Delete web/premiumbusiness-mail directory

--- web/premiumbusiness-mail/db-mailserver.php
+++
@@ -1,2 +0,0 @@
-<?php
-$o_sql = new mysqli("localhost", "root", "", "mailserver");
--- web/premiumbusiness-mail/public/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-if (empty($_SESSION['b_logged_in'])) {
-	header("Location: login.php");
-	exit;
-}
-
-require 'html/index.html';
-?>
