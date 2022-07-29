18e4eebb8789e653b704a5842926709de02e1677
WebDevMike2018 <75060355+WebDevMike2018@users.noreply.github.com>
2022-07-29 16:20:06 +0800

    Delete web/lanthirey/php directory

--- web/lanthirey/php/link.php
+++
@@ -1,6 +0,0 @@
-<?php
-$sServerName = 'localhost';
-$sUserName = 'root';
-$sPassword = '';
-$sDatabaseName = 'lanthirey_lanthirey';
-$link = mysqli_connect($sServerName, $sUserName, $sPassword, $sDatabaseName);
